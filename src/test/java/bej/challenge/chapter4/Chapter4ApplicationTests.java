7ed4b85ad02e99794aa00c4a916b923b48ee684b
Husnul Hotimah <husnulhotimah472@gmail.com>
2023-04-16 00:03:37 +0700

    upload project

---
+++ src/test/java/bej/challenge/chapter4/Chapter4ApplicationTests.java
@@ -0,0 +1,13 @@
+package bej.challenge.chapter4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chapter4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
